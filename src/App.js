35a4b1057be8493addfb2211b0a6efc5087756bf
Prashant Uniyal <prashantuniyal.cu@gmail.com>
2017-08-23 15:38:49 +0530

    'MyFirstCommit'

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header title="Welcome to React JDC Training" onHeaderClick={this.onHeaderClick} />
+        <p className="App-intro">
+        "Jaipur is awesome"
+        </p>
+      </div>
+    );
+        
+  }
+    onHeaderClick = event => {
+    event.preventDefault();
+    window.alert('You pressed the header');
+  };
+}
+
+export default App;
